c462a36148e722f303aa4a4cc9284b59302e3a77
hwOktober14 <igor-lidere42@yandex.ru>
2022-10-23 23:35:51 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        task1();
+
+
+    }
+//    Домашнее задание 1, Задача 1
+//    С помощью цикла for выведите в консоль все числа от 1 до 10.
+    public static void task1() {
+}
